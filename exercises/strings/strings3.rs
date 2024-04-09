e8552388ea71bc6affa2f90258a23593259660ee
YngNs <108976255+YngNs@users.noreply.github.com>
2024-04-09 09:06:54 +0000

    modified:   exercises/strings/strings3.rs 	modified:   exercises/strings/strings4.rs

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,26 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    input.trim_a_string()
+    let mut str1 = input.to_string();
+    let str2 = str1.trim();
+    str2.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut str_ = input.to_string();
+    str_.push_str(" world!");
+    str_
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s1 = input.replace("cars","balloons");
+    s1
 }
 
 #[cfg(test)]
